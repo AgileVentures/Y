c8a6e003e5ee3848fff2ac4a1725e8a65ba044dc
Will White <w.white9@icloud.com>
2018-03-02 13:20:28 +0000

    outlined expected proportion test, organised invalid proportion tests, both payer and payee need to read proportion

--- test/Y.test.js
+++ test/Y.test.js
@@ -15,6 +15,9 @@ contract("Y", accounts => {
     y = await Y.new(25, 100, { from: payee });
   });
 
+  // The donation proportion can only be between 0 and 100% exclusive.
+  // If all the functions that can change the donation proportion throw if they're passed an invalid donation proportion, we can be sure that the donation proportion cannot be invalid.
+
   it("throws if you try to deploy with a donation proportion of 0%", () => {
     assert.isRejected(Y.new(0, 100));
   });
@@ -23,10 +26,27 @@ contract("Y", accounts => {
     assert.isRejected(Y.new(100, 100));
   });
 
-  it("lets you read the donation proportion", async () => {
-    const [num, denom] = await y.donationProportion();
+  it("throws if the payee tries to set a donation proportion of 0%", () => {
+    assert.isRejected(y.setDonationProportion(0, 100, { from: payee }));
+  });
+
+  it("throws if the payee tries to set a donation proportion of >= 100%", () => {
+    assert.isRejected(y.setDonationProportion(100, 100, { from: payee }));
+  });
+
+  // Both payer and payee can get the donation proportion.
+
+  it("lets the payer read the donation proportion", async () => {
+    const [num, denom] = await y.donationProportion({ from: payer });
     assert.isTrue(num.equals(25) && denom.equals(100));
   });
+
+  it("lets the payee read the donation proportion", async () => {
+    const [num, denom] = await y.donationProportion({ from: payee });
+    assert.isTrue(num.equals(25) && denom.equals(100));
+  });
+
+  // Only the payee can set the proportion of the payment that will be donated.
 
   it("lets the payee set the donation proportion", async () => {
     const result = await y.setDonationProportion(10, 100, { from: payee }); // TODO different proportion to what's already on the contract
@@ -34,17 +54,16 @@ contract("Y", accounts => {
     assert.isTrue(num.equals(10) && denom.equals(100));
   });
 
-  it("throws if you try to set a donation proportion of 0%", () => {
-    assert.isRejected(y.setDonationProportion(0, 100, { from: payee }));
-  });
-
-  it("throws if you try to set a donation proportion of >= 100%", () => {
-    assert.isRejected(y.setDonationProportion(100, 100, { from: payee }));
-  });
-
   it("throws if someone other than the payee tries to set the donation proportion", () => {
     assert.isRejected(y.setDonationProportion(1, 100, { from: payer }));
   });
+
+  // The donation is what the payer expected it would be.
+  // The donation proportion can be changed between the payer calling to pay and the function executing.
+
+  // Test that the function throws if the donation proportion isn't what the payer is expecting.
+
+  // The payer can pay (payment - donation) and donate (payment * donation proportion expected by payer).
 
   // it("should change the balances as expected", async () => {
   //   const oneEther = web3.toWei(1, "ether");
