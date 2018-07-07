6138d47e537e42cc4269dad3153ba40f0c1ce41d
Will White <w.white9@icloud.com>
2018-07-07 22:18:02 +0100

    pay and donate with GUI

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -1,6 +1,8 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 main =
@@ -12,17 +14,78 @@ main =
         }
 
 
+port payAndDonate :
+    { percent : String
+    , donee : String
+    , ether : String
+    }
+    -> Cmd msg
+
+
+port txID : (String -> msg) -> Sub msg
+
+
+type Msg
+    = PayAndDonate
+    | Ether String
+    | Donee String
+    | TxID String
+
+
+type alias Model =
+    { percent : String, donee : String, ether : String, txID : String }
+
+
 init percent =
-    ( percent, Cmd.none )
+    ( { percent = percent
+      , donee = ""
+      , ether = ""
+      , txID = ""
+      }
+    , Cmd.none
+    )
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
-    ( model, Cmd.none )
+    case message of
+        Ether ether ->
+            ( { model | ether = ether }, Cmd.none )
+
+        Donee donee ->
+            ( { model | donee = donee }, Cmd.none )
+
+        PayAndDonate ->
+            ( model
+            , payAndDonate
+                { percent = model.percent
+                , donee = model.donee
+                , ether = model.ether
+                }
+            )
+
+        TxID txID ->
+            ( { model | txID = txID }, Cmd.none )
 
 
 view model =
-    text model
+    case model.txID of
+        "" ->
+            div []
+                [ div [] [ text (model.percent ++ "% donation") ]
+                , Html.form [ onSubmit PayAndDonate ]
+                    [ label []
+                        [ text "Donate to"
+                        , input [ placeholder "Ethereum address", onInput Donee ] []
+                        ]
+                    , label [] [ text "Pay", input [ onInput Ether ] [], text "Ether" ]
+                    , button [{- type = submit? -}] [ text "Pay And Donate" ]
+                    ]
+                ]
+
+        _ ->
+            div [] [ text ("Paid. Transaction ID: " ++ model.txID) ]
 
 
 subscriptions model =
-    Sub.none
+    txID TxID
