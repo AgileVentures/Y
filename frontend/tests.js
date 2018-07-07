22a3ec98a04aa33e11397649bba78d8dcbc68b57
Will White <w.white9@icloud.com>
2018-07-07 19:24:10 +0100

    donationPercent, payAndDonate functions

---
+++ frontend/tests.js
@@ -0,0 +1,4 @@
+// donationPercent() returns Promise "x%"
+// payAndDonate(percent, donee, ether) returns Promise ...
+// proportionToPercent(random proportions) return correct percents
+// percentToProportion(random percents) return correct proportions
